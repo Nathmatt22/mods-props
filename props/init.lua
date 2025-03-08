d5ecbf2e5103ac866fa3017c94036845868c8dcd
thefire <132939286+Nathmatt22@users.noreply.github.com>
2025-03-08 13:15:53 +0100

    Add files via upload

--- props/init.lua
+++ props/init.lua
@@ -21,7 +21,7 @@ minetest.register_node("props:statue", {
 })
 
 minetest.register_node("props:flag", {
-    description = "Statue",
+    description = "table",
     drawtype = "mesh",
     mesh = "flag.obj",
     tiles = {"flag.png"},
@@ -39,5 +39,27 @@ minetest.register_node("props:flag", {
     selection_box = {
         type = "fixed",
         fixed = {-0.3, -0.2, -0.3, 0.1, 0.7, 0.3}, -- Taille d’un bloc classique
+    },
+})
+
+minetest.register_node("props:table", {
+    description = "table",
+    drawtype = "mesh",
+    mesh = "table.obj",
+    tiles = {"table.png"},
+    groups = {cracky = 3},
+    
+    paramtype = "light",
+    paramtype2 = "facedir",
+    
+      -- Même hitbox que la pierre
+      collision_box = {
+        type = "regular",
+    },
+
+    -- Permet au joueur de sélectionner correctement le bloc
+    selection_box = {
+        type = "fixed",
+        fixed = {-0.5, 0.5, -0.5, 0.5, -0.5, 0.5}, -- Taille d’un bloc classique
     },
 })
